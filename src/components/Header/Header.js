c9e0eeb74b7abbfd078deb4598b902eb8128b804
PhilippeShi <philippeshi17@gmail.com>
2021-12-25 22:42:03 -0500

    Header done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,12 +3,46 @@ import React from 'react';
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
 import { DiCssdeck } from 'react-icons/di';
 
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
+import { Span, Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
 
 const Header = () =>  (
-  <div>
-    Header
-  </div>
+  <Container>
+    <Div1>
+      <Link href="">
+        <a style={{ display: "flex", alignItems: "center", color: "white", marginBottom: "20px"}}>
+          <DiCssdeck size="3rem" /> <Span>Portfolio</Span>
+        </a>
+      </Link>
+    </Div1>
+    <Div2>
+      <li>
+        <Link href="#Projects">
+          <NavLink>Projects</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#tech">
+          <NavLink>Technologies</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#About">
+          <NavLink>About</NavLink>
+        </Link>
+      </li>
+    </Div2>
+    <Div3>
+      <SocialIcons target="_blank" href="https://github.com/PhilippeShi">
+        <AiFillGithub size="3rem" />
+      </SocialIcons>
+      <SocialIcons target="_blank" href="https://www.linkedin.com/in/philippe-shi-a7a51920b/">
+        <AiFillLinkedin size="3rem" />
+      </SocialIcons>
+      <SocialIcons target="_blank" href="https://www.instagram.com/philippe.shi/">
+        <AiFillInstagram size="3rem" />
+      </SocialIcons>
+    </Div3>
+  </Container>
 );
 
 export default Header;
--- src/components/Header/HeaderStyles.js
+++ src/components/Header/HeaderStyles.js
@@ -17,6 +17,11 @@ export const Container = styled.div`
     grid-row-gap: 0.5rem;
   }
 `;
+
+export const Span = styled.span`
+  font-size: 2rem;
+`;
+
 export const Div1 = styled.div`
   grid-area: 1 / 1 / 2 / 2;
   display: flex;
@@ -111,6 +116,7 @@ export const NavProductsIcon = styled(IoIosArrowDropdown)`
 // Social Icons 
 
 export const SocialIcons = styled.a`
+target="_blank";
 transition: 0.3s ease;
 color: white;
 border-radius: 50px;
